2b3e7551c116f7929568e3fa88a570fcadad0709
Art <artem.goncharenko@gmail.com>
2017-01-20 20:42:31 +0100

    Child entity created

--- Friendly-plans/app/src/main/java/database/DatabaseCreator.java
+++ Friendly-plans/app/src/main/java/database/DatabaseCreator.java
@@ -1,14 +1,30 @@
 package database;
 
 import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 
 public class DatabaseCreator {
     public static void main(String[] args) throws Exception {
         Schema schema = new Schema(1086, "database");
+        createChild(schema);
+
         new DaoGenerator().generateAll(schema, "./app/src/main/java");
     }
+
+    private static Entity createChild(Schema schema) {
+        Entity child = schema.addEntity("Child");
+        child.addIdProperty();
+        child.addStringProperty("name").notNull();
+        child.addStringProperty("surname").notNull();
+        child.addStringProperty("font_size");
+        child.addStringProperty("picture_size");
+        child.addStringProperty("display_mode");
+
+        return child;
+    }
+   //TODO: create other entities and relations
 }
 
 
