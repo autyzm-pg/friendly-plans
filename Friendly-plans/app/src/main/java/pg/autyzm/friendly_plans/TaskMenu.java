d9be072227afb200cd39284ad52a1b56638aa085
msolecka <mal.solecka@gmail.com>
2017-02-22 22:29:23 +0100

    #69: Add basic xml mock of screen for create task and change main  activity to connect it with application

---
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskMenu.java
@@ -0,0 +1,15 @@
+package pg.autyzm.friendly_plans;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskMenu extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.task_menu, container, false);
+    }
+}
