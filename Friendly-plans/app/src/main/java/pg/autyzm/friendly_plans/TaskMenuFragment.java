0993b90f3b0038abb81d27dc0ca31c683769e7fb
malsolec <mal.solecka@gmail.com>
2017-02-24 17:39:28 +0100

    #69: Rename to fit code style

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskMenuFragment.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskMenuFragment.java
@@ -6,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TaskContainer extends Fragment {
+public class TaskMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.task_container, container, false);
+        return inflater.inflate(R.layout.fragment_task_menu, container, false);
     }
 }
