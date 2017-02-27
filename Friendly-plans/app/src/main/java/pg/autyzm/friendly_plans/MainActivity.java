063fc50d9721caccb1096ef96048c16d49cbc435
Artem3 <artem.goncharenko@gmail.com>
2017-02-27 17:33:08 +0100

    #70 OnlickListener added to MainActivity, with log output. Google Code Style Guidlines installed and code format has been changed.

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/MainActivity.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/MainActivity.java
@@ -2,11 +2,43 @@ package pg.autyzm.friendly_plans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_management);
-    }
+
+  private static final String TAG = "MainActivity.java";
+
+  EditText taskName;
+  EditText taskPicture;
+  EditText taskSound;
+  EditText taskDurTime;
+  Button taskNext;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.task_management);
+
+    taskName = (EditText) findViewById(R.id.id_et_task_name);
+    taskPicture = (EditText) findViewById(R.id.id_et_task_picture);
+    taskSound = (EditText) findViewById(R.id.id_et_task_sound);
+    taskDurTime = (EditText) findViewById(R.id.id_et_task_duration_time);
+    taskNext = (Button) findViewById(R.id.id_btn_task_next);
+
+    taskNext.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+
+        // temporary solution
+        taskName.getText().toString();
+        Log.i("taskName :", "{" + taskName.getText().toString() + "}");
+        Log.i("taskPicture :", "{" + taskSound.getText().toString() + "}");
+        Log.i("taskSound :", "{" + taskDurTime.getText().toString() + "}");
+        Log.i("taskDurTime :", "{" + taskDurTime.getText().toString() + "}");
+      }
+    });
+  }
 }
