1a699dd657aa31bcce388d20b37e7da36ee1a7c0
agoncharenko <agoncharenko@pl.sii.eu>
2017-02-28 22:26:00 +0100

    code updated according Codacy recomendation

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/MainActivity.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/MainActivity.java
@@ -1,7 +1,7 @@
 package pg.autyzm.friendly_plans;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,36 +9,36 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final String TAG = "MainActivity.java";
+    public static final String TAG = "MainActivity.java";
 
-  EditText taskName;
-  EditText taskPicture;
-  EditText taskSound;
-  EditText taskDurTime;
-  Button taskNext;
+    private EditText taskName;
+    private EditText taskPicture;
+    private EditText taskSound;
+    private EditText taskDurTime;
+    private Button taskNext;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.task_management);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-    taskName = (EditText) findViewById(R.id.id_et_task_name);
-    taskPicture = (EditText) findViewById(R.id.id_et_task_picture);
-    taskSound = (EditText) findViewById(R.id.id_et_task_sound);
-    taskDurTime = (EditText) findViewById(R.id.id_et_task_duration_time);
-    taskNext = (Button) findViewById(R.id.id_btn_task_next);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_management);
 
-    taskNext.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
+        taskName = (EditText) findViewById(R.id.id_et_task_name);
+        taskPicture = (EditText) findViewById(R.id.id_et_task_picture);
+        taskSound = (EditText) findViewById(R.id.id_et_task_sound);
+        taskDurTime = (EditText) findViewById(R.id.id_et_task_duration_time);
+        taskNext = (Button) findViewById(R.id.id_btn_task_next);
 
-        // temporary solution
-        taskName.getText().toString();
-        Log.i("taskName :", "{" + taskName.getText().toString() + "}");
-        Log.i("taskPicture :", "{" + taskSound.getText().toString() + "}");
-        Log.i("taskSound :", "{" + taskDurTime.getText().toString() + "}");
-        Log.i("taskDurTime :", "{" + taskDurTime.getText().toString() + "}");
-      }
-    });
-  }
+        taskNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // temporary solution
+                taskName.getText().toString();
+                Log.i("taskName :", "{" + taskName.getText().toString() + "}");
+                Log.i("taskPicture :", "{" + taskPicture.getText().toString() + "}");
+                Log.i("taskSound :", "{" + taskSound.getText().toString() + "}");
+                Log.i("taskDurTime :", "{" + taskDurTime.getText().toString() + "}");
+            }
+        });
+    }
 }
