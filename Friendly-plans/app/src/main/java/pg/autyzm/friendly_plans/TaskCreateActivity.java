2038fe1ff60dd7a3a4f2865c7b306625af68bff9
Michal Gruca <michal.gruca@gmail.com>
2017-04-16 19:18:07 +0200

    74 adding notification management
    adding simple class displaing messages in form of toast.
    Following assumption has been made
    * We will be using Toast as only source of displaying messages
    ** if this should ever change, it's easy to extract interface and provide specialized implementations for other cases
    * We want messages to be I18n friendly. The method is accepting R.string codes
    ** may be simply overladed if needed
    * We don't want to introduce specialized super activity/fragment class.
    ** However it could be a good idea, to provide template method, that would accept only code/message string, and took care about handling context, or other parameters if they would ever need to be introduced

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskCreateActivity.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskCreateActivity.java
@@ -2,12 +2,22 @@ package pg.autyzm.friendly_plans;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import pg.autyzm.friendly_plans.notifications.ToastUserNotifier;
 
 public class TaskCreateActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_management);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.task_management);
+	}
+
+	public void eventCreateTask(View view) {
+		showSuccessMessage();
+	}
+
+	private void showSuccessMessage() {
+		ToastUserNotifier.displayNotifications(R.string.task_saved_message, getApplicationContext());
+	}
 }
