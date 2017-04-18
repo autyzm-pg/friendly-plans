246ac08a63dbc871869347dc3905b818e112168f
Michal Gruca <michal.gruca@gmail.com>
2017-04-18 18:15:20 +0200

    Fixing formatting
    imported google style guide and reformatted code to follow guideline

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/notifications/ToastUserNotifier.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/notifications/ToastUserNotifier.java
@@ -5,10 +5,10 @@ import android.widget.Toast;
 
 public class ToastUserNotifier {
 
-	public static void displayNotifications(int messageCode, Context context){
-		int duration = Toast.LENGTH_SHORT;
-		Toast toast = Toast.makeText(context, messageCode, duration);
+  public static void displayNotifications(int messageCode, Context context) {
+    int duration = Toast.LENGTH_SHORT;
+    Toast toast = Toast.makeText(context, messageCode, duration);
 
-		toast.show();
-	}
+    toast.show();
+  }
 }
