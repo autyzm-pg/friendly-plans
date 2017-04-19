be37f78a35710f9cfca94efb04933ac8c2ec9e51
Michal Gruca <michal.gruca@gmail.com>
2017-04-19 05:27:23 +0200

    Further fixes to issues found by codacy
    Codacy still complains about indents, however that does not seem to be in line with google style

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskCreateActivity.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskCreateActivity.java
@@ -7,17 +7,19 @@ import pg.autyzm.friendly_plans.notifications.ToastUserNotifier;
 
 public class TaskCreateActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.task_management);
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.task_management);
+  }
 
-	public void eventCreateTask(View view) {
-		showSuccessMessage();
-	}
+  public void eventCreateTask(View view) {
+    showSuccessMessage();
+  }
 
-	private void showSuccessMessage() {
-		ToastUserNotifier.displayNotifications(R.string.task_saved_message, getApplicationContext());
-	}
+  private void showSuccessMessage() {
+    ToastUserNotifier.displayNotifications(
+        R.string.task_saved_message,
+        getApplicationContext());
+  }
 }
--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/notifications/ToastUserNotifier.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/notifications/ToastUserNotifier.java
@@ -5,6 +5,8 @@ import android.widget.Toast;
 
 public class ToastUserNotifier {
 
+  private ToastUserNotifier(){}
+
   public static void displayNotifications(int messageCode, Context context) {
     int duration = Toast.LENGTH_SHORT;
     Toast toast = Toast.makeText(context, messageCode, duration);
