add524f13c68ab942205c78534b8ea3a0ed13185
Mateusz <mateuszgabrynowicz@gmail.com>
2017-04-26 09:32:22 +0200

    #96 display task list create activity for the mock
    * #96: created RecyclerViewAdapter + simple activity with mocked list
    
    * #96 - Added connection with DB in TaskListActivity. Create tests for the activity. Added RecyclerViewMatcher

---
+++ Friendly-plans/app/src/androidTest/java/pg/autyzm/friendly_plans/TaskListActivityTest.java
@@ -0,0 +1,64 @@
+package pg.autyzm.friendly_plans;
+
+import android.content.Intent;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import database.repository.TaskTemplateRepository;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static matcher.RecyclerViewMatcher.withRecyclerView;
+
+@RunWith(AndroidJUnit4.class)
+public class TaskListActivityTest {
+    @ClassRule
+    public static DaoSessionResource daoSessionResource = new DaoSessionResource();
+    @Rule
+    public ActivityTestRule<TaskListActivity> activityRule = new ActivityTestRule<>(
+            TaskListActivity.class, true, true);
+    private static final String expectedName = "TEST TASK ";
+
+    @Before
+    public void setUp() {
+        final int numberOfTasks = 10;
+        TaskTemplateRepository taskTemplateRepository = new TaskTemplateRepository(
+                daoSessionResource.getSession(activityRule.getActivity().getApplicationContext()));
+        for (int taskNumber = 0; taskNumber < numberOfTasks; taskNumber++) {
+            taskTemplateRepository
+                    .create(expectedName + taskNumber,
+                            taskNumber, (long) taskNumber,
+                            (long) taskNumber);
+        }
+        activityRule.launchActivity(new Intent());
+    }
+
+    @Test
+    public void checkIfItemsAreClickable() {
+        final int testedTaskPosition = 3;
+        onView(withId(R.id.rv_task_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(testedTaskPosition, click()));
+    }
+
+    @Test
+    public void checkIfTaskIsAddedToDBAndProperlyDisplayedOnRecyclerView() {
+        final int testedTaskPosition = 5;
+        onView(withId(R.id.rv_task_list)).perform(scrollToPosition(testedTaskPosition));
+        onView(withRecyclerView(R.id.rv_task_list)
+                .atPosition(testedTaskPosition))
+                .check(matches(hasDescendant(withText(expectedName
+                        + testedTaskPosition))));
+    }
+}
---
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/TaskListActivity.java
@@ -0,0 +1,44 @@
+package pg.autyzm.friendly_plans;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import javax.inject.Inject;
+
+import database.repository.TaskTemplateRepository;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Inject
+    TaskTemplateRepository taskTemplateRepository;
+    private TaskRecyclerViewAdapter taskListAdapter;
+    private static final String TAG = "TaskListActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((App) getApplication()).getAppComponent().inject(this);
+        setContentView(R.layout.activity_task_list);
+        setUpViews();
+        taskListAdapter.setTaskItems(taskTemplateRepository.getAll());
+    }
+
+    private void setUpViews() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_task_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskListAdapter = new TaskRecyclerViewAdapter(taskItemClickListener);
+        recyclerView.setAdapter(taskListAdapter);
+    }
+
+    TaskRecyclerViewAdapter.TaskItemClickListener taskItemClickListener =
+            new TaskRecyclerViewAdapter.TaskItemClickListener() {
+                @Override
+                public void onTaskItemClick(int position) {
+                    Log.d(TAG, "onTaskItemClick: " + position);
+                }
+            };
+}
