b3bf31370b6e255b7b6d0427453e956cf88e0753
Michał Gruca <michalgruca@gmail.com>
2017-10-15 22:13:59 +0200

    #74 notification on task creation (#149)
    * Issue #67 First version of the main screen
    
    screen XML and i18n related to it.
    Simply added buttons with labels to the screen.
    
    Still missing navigation or any logic. There is no layout as well
    
    * Adapting to android naming guide
    
    Changes to name of resources, extraction of i18n strings
    resolves #67
    
    * Clean up of activities and navigation
    
    new activity showed up in the meantime, for task creation. This commit
    creates a new activity class, clears up navigation between activities
    
    * fixing failing test
    
    fixes #69
    
    * 110 Changes in the layout to add scroll
    
    The main layout is now packed inside the scroll view.
    
    * 74 adding notification management
    
    adding simple class displaying messages in form of toast.
    Following assumption has been made
    * We will be using Toast as the only source of displaying messages
    ** if this should ever change, it's easy to extract interface and provide specialized implementations for other cases
    * We want messages to be I18n friendly. The method is accepting R.string codes
    ** may be simply overloaded if needed
    * We don't want to introduce specialized super activity/fragment class.
    ** However it could be a good idea, to provide template method, that would accept only code/message string, and took care about handling context, or other parameters if they would ever need to be introduced
    
    * Fixing formatting
    
    imported google style guide and reformatted code to follow the guideline

---
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/notifications/ToastUserNotifier.java
@@ -0,0 +1,16 @@
+package pg.autyzm.friendly_plans.notifications;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUserNotifier {
+
+  private ToastUserNotifier(){}
+
+  public static void displayNotifications(int messageCode, Context context) {
+    int duration = Toast.LENGTH_SHORT;
+    Toast toast = Toast.makeText(context, messageCode, duration);
+
+    toast.show();
+  }
+}
