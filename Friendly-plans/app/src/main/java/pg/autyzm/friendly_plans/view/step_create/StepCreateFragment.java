8a94d509fa00d053f874942b72e1a569e3fca9ec
paulamoller <paulina_moller@o2.pl>
2018-02-02 16:44:23 +0100

    131: Remove unused fields

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/step_create/StepCreateFragment.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/step_create/StepCreateFragment.java
@@ -11,8 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import database.repository.AssetRepository;
 import database.repository.StepTemplateRepository;
