92206684af51f9b2494b93a176e779bd5a43ea5c
paulamoller <paulina_moller@o2.pl>
2018-04-19 18:25:00 +0200

    #238: data binding of field plan's name

---
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create/PlanCreateActivityEvents.java
@@ -0,0 +1,7 @@
+package pg.autyzm.friendly_plans.view.plan_create;
+
+import pg.autyzm.friendly_plans.view.plan_create.PlanCreateData;
+
+public interface PlanCreateActivityEvents {
+    void savePlanData(PlanCreateData planCreateData);
+}
--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create/PlanCreateFragment.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create/PlanCreateFragment.java
@@ -1,19 +1,33 @@
 package pg.autyzm.friendly_plans.view.plan_create;
 
 import android.app.Fragment;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import pg.autyzm.friendly_plans.App;
 import pg.autyzm.friendly_plans.R;
+import pg.autyzm.friendly_plans.databinding.FragmentPlanCreateBinding;
 
-public class PlanCreateFragment extends Fragment {
+public class PlanCreateFragment extends Fragment implements PlanCreateActivityEvents{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        ((App) getActivity().getApplication()).getAppComponent().inject(this);
-        return inflater.inflate(R.layout.fragment_plan_create, container, false);
+        FragmentPlanCreateBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_plan_create, container, false);
+
+        View view = binding.getRoot();
+
+        PlanCreateData planData = new PlanCreateData("");
+        binding.setPlanData(planData);
+        binding.setPlanDataClick(this);
+        return view;
     }
+
+    @Override
+    public void savePlanData(PlanCreateData planCreateData) {
+        Log.i("plan name", planCreateData.getPlanName() + " " ); }
 }
