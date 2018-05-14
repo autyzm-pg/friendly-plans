a9a755837ac3950b861ed31b5c58e6a541d217e1
paulamoller <32984376+paulamoller@users.noreply.github.com>
2018-05-14 18:09:32 +0200

    #274: Creating xml mock and empty fragment for tasks list assigned to plan (#276)
    * #274: Creating xml mock and empty fragment
    
    * #274: Adding test

---
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create_task_list/PlanTaskListFragment.java
@@ -0,0 +1,26 @@
+package pg.autyzm.friendly_plans.view.plan_create_task_list;
+
+import android.app.Fragment;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import pg.autyzm.friendly_plans.App;
+import pg.autyzm.friendly_plans.R;
+import pg.autyzm.friendly_plans.databinding.FragmentPlanTaskListBinding;
+
+public class PlanTaskListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ((App) getActivity().getApplication()).getAppComponent().inject(this);
+
+        FragmentPlanTaskListBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_plan_task_list, container, false);
+
+        View view = binding.getRoot();
+        return view;
+    }
+}
