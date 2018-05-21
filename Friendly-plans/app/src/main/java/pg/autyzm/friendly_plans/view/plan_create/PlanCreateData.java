4b4c4fec36d542fe6b8b36b105a6e23390d238d4
Mikołaj <mikolevy1@gmail.com>
2018-05-21 21:05:09 +0200

    #267: Plan name validation (#269)
    * #267: Plan name validation
    
    - Implement PlanValidation based on TaskValidation
    - Check is name valid during Plan creation, display simple toast message
    
    * #267: Add test for duplicated name
    
    * #267: Add validation to not go to tasks list when plan was not created
    
    * #267: Add closing keyboard in plan create test before validating toast

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create/PlanCreateData.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/view/plan_create/PlanCreateData.java
@@ -2,15 +2,18 @@ package pg.autyzm.friendly_plans.view.plan_create;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+
 import pg.autyzm.friendly_plans.BR;
 
 
 public class PlanCreateData extends BaseObservable {
 
     private String planName;
+    private String nameFieldError;
 
     public PlanCreateData(String planName) {
         this.planName = planName;
+        nameFieldError = "";
     }
 
     @Bindable
@@ -21,5 +24,15 @@ public class PlanCreateData extends BaseObservable {
     public void setPlanName(String planName) {
         this.planName = planName;
         notifyPropertyChanged(BR.planName);
+    }
+
+    @Bindable
+    public String getNameFieldError() {
+        return nameFieldError;
+    }
+
+    public void setNameFieldError(String errorMessage) {
+        nameFieldError = errorMessage;
+        notifyPropertyChanged(BR.nameFieldError);
     }
 }
