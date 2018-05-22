004ca645c28cb0ea3c361f8a76f94ba305ae771c
SuperJarek <jaroslaw.szymukowicz@gmail.com>
2018-05-22 22:59:16 +0200

    plan list code beautifying

--- Friendly-plans/app/src/androidTest/java/pg/autyzm/friendly_plans/PlanListActivityTest.java
+++ Friendly-plans/app/src/androidTest/java/pg/autyzm/friendly_plans/PlanListActivityTest.java
@@ -7,10 +7,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.doesNotExis
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 import static pg.autyzm.friendly_plans.matcher.RecyclerViewMatcher.withRecyclerView;
 
