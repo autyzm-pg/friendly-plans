4441a56b7c94c205973e2e8351bc9281548dbfcc
paulamoller <paulina_moller@o2.pl>
2018-05-28 23:56:58 +0200

    #245: adding tests

--- Friendly-plans/app/src/main/java/database/repository/ChildRepository.java
+++ Friendly-plans/app/src/main/java/database/repository/ChildRepository.java
@@ -1,6 +1,7 @@
 package database.repository;
 
 import database.entities.Child;
+import database.entities.ChildDao.Properties;
 import database.entities.DaoSession;
 import java.util.List;
 
@@ -19,6 +20,13 @@ public class ChildRepository {
         return daoSession.getChildDao().insert(childTemplate);
     }
 
+    public List<Child> getBySurname(String surname) {
+        return daoSession.getChildDao()
+                .queryBuilder()
+                .where(Properties.Surname.eq(surname))
+                .list();
+    }
+
     public Child get(Long id) {
         return daoSession.getChildDao().load(id);
     }
@@ -29,5 +37,9 @@ public class ChildRepository {
 
     public void delete(Long id) {
         daoSession.getChildDao().deleteByKey(id);
+    }
+
+    public void deleteAll() {
+        daoSession.getChildDao().deleteAll();
     }
 }
