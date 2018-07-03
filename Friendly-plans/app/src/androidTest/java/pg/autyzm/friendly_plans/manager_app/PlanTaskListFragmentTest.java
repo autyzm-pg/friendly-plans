2203f8b0f09ab7fd22e70734bf4bd9e0689c4b91
Amari111 <baniecki.jan@gmail.com>
2018-07-03 22:08:50 +0200

    test to task deleting from plan task list

--- Friendly-plans/app/src/androidTest/java/pg/autyzm/friendly_plans/manager_app/PlanTaskListFragmentTest.java
+++ Friendly-plans/app/src/androidTest/java/pg/autyzm/friendly_plans/manager_app/PlanTaskListFragmentTest.java
@@ -2,6 +2,7 @@ package pg.autyzm.friendly_plans.manager_app;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
@@ -14,15 +15,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import database.repository.PlanTemplateRepository;
+import database.repository.TaskTemplateRepository;
 import pg.autyzm.friendly_plans.R;
 import pg.autyzm.friendly_plans.manager_app.view.plan_create.PlanCreateActivity;
 import pg.autyzm.friendly_plans.manager_app.view.plan_create_task_list.PlanTaskListFragment;
 import pg.autyzm.friendly_plans.resource.DaoSessionResource;
 import pg.autyzm.friendly_plans.resource.PlanTemplateRule;
 import pg.autyzm.friendly_plans.resource.TaskTemplateRule;
+import pg.autyzm.friendly_plans.view_actions.ViewClicker;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -36,6 +41,11 @@ public class PlanTaskListFragmentTest {
     private static final String PLAN_ID = "PLAN_ID";
     private static final String TASK_NAME = "TASK_NAME";
     private static final String PLAN_NAME = "PLAN NAME";
+    private static final String DELETE_TEST_TASK = "DELETE TEST TASK";
+
+    private  PlanTemplateRepository planTemplateRepository;
+    private  TaskTemplateRepository taskTemplateRepository;
+    private long planId;
 
     @ClassRule
     public static DaoSessionResource daoSessionResource = new DaoSessionResource();
@@ -54,10 +64,16 @@ public class PlanTaskListFragmentTest {
 
     @Before
     public void setUp() {
-        PlanTaskListFragment fragment = new PlanTaskListFragment();
+        Context context = activityRule.getActivity().getApplicationContext();
+        planTemplateRepository = new PlanTemplateRepository(daoSessionResource.getSession(context));
+        taskTemplateRepository = new TaskTemplateRepository(daoSessionResource.getSession(context));
 
+        long taskId = taskTemplateRepository.create(DELETE_TEST_TASK, 1, null, null);
+        planId = planTemplateRule.createPlan(PLAN_NAME);
+        planTemplateRepository.setTasksWithThisPlan(planId, taskId);
         taskTemplateRule.createTask(TASK_NAME);
-        long planId = planTemplateRule.createPlan(PLAN_NAME);
+
+        PlanTaskListFragment fragment = new PlanTaskListFragment();
 
         Bundle args = new Bundle();
         args.putLong(PLAN_ID, planId);
@@ -95,8 +111,27 @@ public class PlanTaskListFragmentTest {
         onView(withId(R.id.id_btn_add_tasks_to_plan))
                 .perform(click());
 
+        recyclerView = (RecyclerView) activityRule.getActivity().findViewById(R.id.rv_create_plan_task_list);
+        lastPosition = recyclerView.getAdapter().getItemCount() - 1;
+
         onView(withRecyclerView(R.id.rv_create_plan_task_list)
-                .atPosition(0))
+                .atPosition(lastPosition))
                 .check(matches(hasDescendant(withText(TASK_NAME))));
+    }
+
+    @Test
+    public void whenRemoveIconClickedExpectTaskToNoLongerBeInThisPlan(){
+        final int testedTaskPosition = 0;
+
+        onView(withId(R.id.rv_create_plan_task_list))
+                .perform(RecyclerViewActions
+                        .actionOnItemAtPosition(testedTaskPosition,
+                                new ViewClicker(R.id.id_remove_task)));
+
+        onView(withId(R.id.rv_create_plan_task_list)).perform(
+                RecyclerViewActions.scrollToPosition(testedTaskPosition));
+
+        onView(withRecyclerView(R.id.rv_create_plan_task_list)
+                .atPosition(testedTaskPosition)).check(doesNotExist());
     }
 }
