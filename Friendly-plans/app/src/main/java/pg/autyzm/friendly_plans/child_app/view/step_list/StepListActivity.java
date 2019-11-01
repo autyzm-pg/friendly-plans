3e8f8ca27717658600a5bbe93e2c977ed7aaa8b6
JanBan <baniecki.jan@gmail.com>
2019-11-02 00:35:09 +0100

    progress2

--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/child_app/view/step_list/StepListActivity.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/child_app/view/step_list/StepListActivity.java
@@ -1,9 +1,11 @@
 package pg.autyzm.friendly_plans.child_app.view.step_list;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ import pg.autyzm.friendly_plans.App;
 import pg.autyzm.friendly_plans.R;
 
 public class StepListActivity extends AppCompatActivity {
+
+    private StepRecyclerViewAdapter stepRecyclerViewAdapter;
 
     @Inject
     StepTemplateRepository stepTemplateRepository;
@@ -34,7 +38,7 @@ public class StepListActivity extends AppCompatActivity {
                 getIntent().getExtras().getLong(ActivityProperties.TASK_ID)
         );
         String filesDirectory = getApplicationContext().getFilesDir().toString();
-        StepRecyclerViewAdapter stepRecyclerViewAdapter = new StepRecyclerViewAdapter(steps, filesDirectory);
+        stepRecyclerViewAdapter = new StepRecyclerViewAdapter(steps, filesDirectory, stepListener);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_child_app_step_list);
         recyclerView.setHasFixedSize(true);
@@ -42,4 +46,39 @@ public class StepListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(stepRecyclerViewAdapter);
     }
+
+    Runnable updater;
+    Integer asd = 0;
+
+    StepRecyclerViewAdapter.StepItemClickListener stepListener = new StepRecyclerViewAdapter.StepItemClickListener() {
+        @Override
+        public void selectStepListener(int position, final TextView stepName) {
+//            while (stepRecyclerViewAdapter.currentStepPosition != 3) {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                stepRecyclerViewAdapter.currentStepPosition++;
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        stepRecyclerViewAdapter.notifyDataSetChanged();
+//                    }
+//                });
+//            }
+//        }
+            final Handler timerHandler = new Handler();
+
+            updater = new Runnable() {
+                @Override
+                public void run() {
+                    stepName.setText(asd.toString());
+                    asd++;
+                    timerHandler.postDelayed(updater, 1000);
+                }
+            };
+            timerHandler.post(updater);
+        }
+    };
 }
--- Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/child_app/view/step_list/StepRecyclerViewAdapter.java
+++ Friendly-plans/app/src/main/java/pg/autyzm/friendly_plans/child_app/view/step_list/StepRecyclerViewAdapter.java
@@ -1,5 +1,10 @@
 package pg.autyzm.friendly_plans.child_app.view.step_list;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +16,7 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import database.entities.StepTemplate;
 import pg.autyzm.friendly_plans.R;
@@ -18,11 +24,55 @@ import pg.autyzm.friendly_plans.R;
 public class StepRecyclerViewAdapter extends RecyclerView.Adapter<StepRecyclerViewAdapter.StepRecyclerViewHolder> {
     private List<StepTemplate> steps;
     private String imageDirectory;
+    public Integer currentStepPosition = -1;
     private StepItemClickListener stepItemClickListener;
 
     protected interface StepItemClickListener {
-        void selectStepListener(int position);
+        void selectStepListener(int position, TextView stepName);
     }
+
+    public void test(int position){
+        if (position == currentStepPosition + 1)
+            currentStepPosition = position;
+        notifyDataSetChanged();
+    }
+//
+//    private StepItemClickListener stepItemClickListener = new StepItemClickListener(){
+//
+//        @Override
+//        public void selectStepListener(int position) {
+//            new Handler(Looper.getMainLooper()).post(new Runnable() {
+//                public void run() {
+//                    while(currentStepPosition != 3){
+//                        try {
+//                            Thread.sleep(1500);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                        currentStepPosition++;
+//                        notifyDataSetChanged();
+//                    }
+//                }
+//            });
+//            if (position == currentStepPosition + 1)
+//                currentStepPosition = position;
+//            notifyDataSetChanged();
+//            Executors.newSingleThreadExecutor().execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    while(currentStepPosition != 3){
+//                        try {
+//                            Thread.sleep(1500);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                        currentStepPosition++;
+//                        notifyDataSetChanged();
+//                    }
+//                }
+//            });
+//        }
+//    };
 
     static class StepRecyclerViewHolder extends RecyclerView.ViewHolder {
         TextView stepName;
@@ -30,11 +80,12 @@ public class StepRecyclerViewAdapter extends RecyclerView.Adapter<StepRecyclerVi
         String imageDirectory;
         StepItemClickListener stepItemClickListener;
 
-        StepRecyclerViewHolder(View itemView, String imageDirectory) {
+        StepRecyclerViewHolder(View itemView, String imageDirectory, StepItemClickListener stepItemClickListener) {
             super(itemView);
             this.stepName = (TextView) itemView.findViewById(R.id.id_tv_step_name);
             this.stepImage = (ImageView) itemView.findViewById(R.id.id_iv_step_image);
             this.imageDirectory = imageDirectory;
+            this.stepItemClickListener = stepItemClickListener;
             itemView.setOnClickListener(stepItemListener);
         }
 
@@ -53,19 +104,16 @@ public class StepRecyclerViewAdapter extends RecyclerView.Adapter<StepRecyclerVi
         View.OnClickListener stepItemListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stepItemClickListener.selectStepListener(getAdapterPosition());
+                stepItemClickListener.selectStepListener(getAdapterPosition(), stepName);
             }
         };
     }
 
-    StepRecyclerViewAdapter(List<StepTemplate> steps, String imageDirectory)
+    StepRecyclerViewAdapter(List<StepTemplate> steps, String imageDirectory, StepItemClickListener stepItemClickListener)
     {
         this.steps = steps;
         this.imageDirectory = imageDirectory;
-    }
-
-    public void asd(){
-
+        this.stepItemClickListener = stepItemClickListener;
     }
 
     @Override
@@ -73,7 +121,7 @@ public class StepRecyclerViewAdapter extends RecyclerView.Adapter<StepRecyclerVi
                                                                              int viewType) {
         View stepView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.child_app_item_step, parent, false);
-        return new StepRecyclerViewAdapter.StepRecyclerViewHolder(stepView, imageDirectory);
+        return new StepRecyclerViewAdapter.StepRecyclerViewHolder(stepView, imageDirectory, stepItemClickListener);
     }
 
     @Override
@@ -82,6 +130,14 @@ public class StepRecyclerViewAdapter extends RecyclerView.Adapter<StepRecyclerVi
             StepTemplate step = steps.get(position);
             holder.setUpHolder(step);
         }
+        if (currentStepPosition != null && position == currentStepPosition)
+            holder.itemView.setBackgroundColor(Color.parseColor("#8BF600"));
+        else {
+            holder.itemView.setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        if (position < currentStepPosition)
+            holder.stepName.setPaintFlags(holder.stepName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
     }
 
     @Override
